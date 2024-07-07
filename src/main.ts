7a82647ee6664509b0abd919017ab7628e834ba0
adrear <kirillmanakhov2306@gmail.com>
2024-07-07 11:30:08 +0300

    init nest

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Configuration } from './config/configuration.interface';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get<ConfigService<Configuration>>(ConfigService);
+    const port = configService.get<number>('port');
+    await app.listen(port);
+}
+bootstrap();
